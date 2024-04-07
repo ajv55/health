9214de2dfda19d06d3c4ca0f883b62e84e45bd13
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-04-06 20:48:29 -0400

    adding the why section

--- app/components/mainPage/why.tsx
+++ app/components/mainPage/why.tsx
@@ -10,26 +10,48 @@ export default function Why() {
 
         <h1 className='text-6xl text-white ml-10 mt-44 text-start w-[90%] font-bold tracking-wider'><span className=' font-extrabold bg-gradient-to-br from-amber-900 via-amber-500 to-amber-300 bg-clip-text text-transparent '>Unlocking </span>Your Weight Loss Success: <br /> <span className='font-extrabold bg-gradient-to-br from-red-900 via-red-400 to-red-200 bg-clip-text text-transparent'>Here&#39;s How!</span></h1>
         <div className='border-2 w-full h-content flex flex-col justify-center'>
-          <div className=' drop-shadow-xl border-4 border-orange-400 w-full h-[23rem] flex justify-evenly items-center relative'>
-          <h1 className='text-[19rem] text-white -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>1</h1>
-              <h1 className='text-5xl text-center bg-gradient-to-br  from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent border font-bold tracking-wide'>Science-Backed Approach</h1>
+          <div className=' drop-shadow-xl  w-full h-[23rem] flex justify-evenly items-center gap-4 relative'>
+            <div className=' relative'>
+              <h1 className='text-[30rem] text-white -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>1</h1>
+              <h1 className='text-5xl text-center bg-gradient-to-br  from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent  font-bold tracking-wide'>Science-Backed Approach</h1>
+            </div>
+              <p className='text-3xl text-center text-white tracking-wide font-light w-[65%]'>At WeightTrack, <span className='font-bold text-4xl  bg-gradient-to-tl from-rose-700 via-orange-200 to-rose-600 bg-clip-text text-transparent'>we prioritize science and evidence-based methods</span> to help you achieve your weight loss goals. Our approach is <span className='font-bold text-3xl bg-gradient-to-br from-orange-200 via-red-300 to-red-700 bg-clip-text text-transparent'>rooted in research</span>, ensuring that every feature and recommendation is backed by scientific principles. Trust us to provide you with reliable information and <span className='font-bold text-3xl bg-gradient-to-br from-red-300 via-red-400 to-blue-900 bg-clip-text text-transparent'>effective strategies for sustainable weight loss</span>.</p>
+          </div>
+
+          {/* span
+             className='font-bold text-3xl bg-gradient-to-tr from-emerald-300 via-emerald-500 to-emerald-900 bg-clip-text text-transparent'
+          */}
+
+          {/* second reason */}
+          <div className=' drop-shadow-xl  w-full h-[23rem] flex flex-row-reverse justify-evenly gap-4 items-center relative'>
+              <div className='relative'>
+                <h1 className='text-[30rem] text-white -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>2</h1>
+                <h1 className='text-5xl text-center bg-gradient-to-tl  from-red-800 via-red-500 to-yellow-300 bg-clip-text text-transparent  font-bold tracking-wide'>Personalized Experience</h1>
+              </div>
+              <p className='text-3xl text-center text-white tracking-wide font-light w-[65%] px-3'>We understand that everyone&#39;s weight loss <span className='font-bold text-4xl bg-gradient-to-tr from-blue-300 via-orange-500 to-orange-900 bg-clip-text text-transparent'>journey is unique</span>. That&#39;s why we offer a personalized experience <span className='font-bold text-4xl bg-gradient-to-tr from-blue-300 via-orange-400 to-orange-900 bg-clip-text text-transparent'>tailored to your individual needs</span> and preferences. From customized calorie targets to personalized workout plans, we <span className='font-bold text-4xl bg-gradient-to-bl from-stone-300 via-red-300 to-zinc-300 bg-clip-text text-transparent'>empower you to make choices</span> that align with your goals and lifestyle. With WeightTrack, you&#39;re not just a number &#45; you&#39;re a valued member of our community.</p>
+          </div>
+            {/* third reason */}
+          <div className=' drop-shadow-xl  w-full h-[23rem] flex justify-evenly items-center relative'>
+            <div className='relative'>
+              <h1 className='text-[30rem] text-white -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>3</h1>
+              <h1 className='text-5xl text-center bg-gradient-to-br  from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent  font-bold tracking-wide'>Comprehensive Support</h1>
+            </div>
+              <p className='text-3xl text-left text-white tracking-wide font-light w-[65%]'>Weight loss can be challenging, <span className='font-extrabold text-4xl bg-gradient-to-br from-amber-800 via-amber-500 to-amber-300 bg-clip-text text-transparent'>but you don&#39;t have to go it alone</span>. With WeightTrack, <span className='font-extrabold text-4xl bg-gradient-to-tr from-indigo-900 via-indigo-300 to-indigo-200 bg-clip-text text-transparent'>you&#39;ll have access to comprehensive support</span> every step of the way. Whether you have questions about nutrition, need motivation to stick to your workout routine, or simply want to connect with like-minded individuals, our team and community are here to support you. <span className='font-extrabold text-4xl bg-gradient-to-tr from-amber-800 via-amber-500 to-amber-200 bg-clip-text text-transparent'>We&#39;re committed to your success</span> and will do whatever it takes to help you reach your goals.</p>
+          </div>
+            {/* fourth reason */}
+          <div className=' drop-shadow-xl  w-full h-[23rem] flex justify-evenly items-center relative'>
+          <h1 className='text-[19rem] text-white -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>4</h1>
+              <h1 className='text-5xl text-center bg-gradient-to-br  from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent border font-bold tracking-wide'> User-Friendly Interface</h1>
               <p className='text-2xl text-left text-white tracking-wide font-light w-[65%]'>At WeightTrack, <span className='font-bold  bg-gradient-to-tr from-rose-300 via-pink-500 to-blue-900 bg-clip-text text-transparent'>we prioritize science and evidence-based methods</span> to help you achieve your weight loss goals. Our approach is rooted in research, ensuring that every feature and recommendation is backed by scientific principles. Trust us to provide you with reliable information and effective strategies for sustainable weight loss.</p>
           </div>
 
-          <div className=' drop-shadow-xl border-4 border-orange-400 w-full h-[20rem] flex relative'>
-              <h1 className='text-4xl bg-gradient-to-br from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent border relative flex justify-center items-center w-[50%] font-bold tracking-wide'><h1 className='text-[16rem] text-red-800 -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>1</h1>Science-Backed Approach</h1>
-              <p className='text-2xl text-center text-white w-[90%]  flex justify-center items-center tracking-wide font-light '>At WeightTrack, we prioritize science and evidence-based methods to help you achieve your weight loss goals. Our approach is rooted in research, ensuring that every feature and recommendation is backed by scientific principles. Trust us to provide you with reliable information and effective strategies for sustainable weight loss.</p>
-          </div>
+            {/* fifht reason */}
+            <div className=' drop-shadow-xl  w-full h-[23rem] mb-64 flex justify-evenly items-center relative'>
+            <h1 className='text-[19rem] text-white -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>5</h1>
+                <h1 className='text-5xl text-center bg-gradient-to-br  from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent border font-bold tracking-wide'> Positive Results</h1>
+                <p className='text-2xl text-left text-white tracking-wide font-light w-[65%]'>At WeightTrack, <span className='font-bold  bg-gradient-to-tr from-rose-300 via-pink-500 to-blue-900 bg-clip-text text-transparent'>we prioritize science and evidence-based methods</span> to help you achieve your weight loss goals. Our approach is rooted in research, ensuring that every feature and recommendation is backed by scientific principles. Trust us to provide you with reliable information and effective strategies for sustainable weight loss.</p>
+            </div>
 
-          <div className=' drop-shadow-xl border-4 border-orange-400 w-full h-[20rem] flex relative'>
-              <h1 className='text-4xl bg-gradient-to-br from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent border relative flex justify-center items-center w-[50%] font-bold tracking-wide'><h1 className='text-[16rem] text-red-800 -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>1</h1>Science-Backed Approach</h1>
-              <p className='text-2xl text-center text-white w-[90%] flex justify-center items-center tracking-wide font-light '>At WeightTrack, we prioritize science and evidence-based methods to help you achieve your weight loss goals. Our approach is rooted in research, ensuring that every feature and recommendation is backed by scientific principles. Trust us to provide you with reliable information and effective strategies for sustainable weight loss.</p>
-          </div>
-
-          <div className=' drop-shadow-xl mb-64 border-4 border-orange-400 w-full h-[20rem] flex relative'>
-              <h1 className='text-4xl bg-gradient-to-br from-teal-800 via-red-500 to-yellow-300 bg-clip-text text-transparent border relative flex justify-center items-center w-[50%] font-bold tracking-wide'><h1 className='text-[16rem] text-red-800 -z-10 opacity-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>1</h1>Science-Backed Approach</h1>
-              <p className='text-2xl text-center text-white w-[90%] flex justify-center items-center tracking-wide font-light '>At WeightTrack, we prioritize science and evidence-based methods to help you achieve your weight loss goals. Our approach is rooted in research, ensuring that every feature and recommendation is backed by scientific principles. Trust us to provide you with reliable information and effective strategies for sustainable weight loss.</p>
-          </div>
         </div>
     </div>
   )
