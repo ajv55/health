a1da4130c895cc59c1976e5b38389a54eee76fc1
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-04-26 02:33:55 -0400

    updated the sign page

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -39,16 +39,16 @@ export default function Page() {
 
         <form className=" bg-gradient-to-tr from-teal-300 via-teal-600 to-teal-200 w-[50%] h-screen p-3 flex flex-col justify-bewteen items-center gap-5  drop-shadow-sm"  ref={ref} onSubmit={handleSubmit}>
         <h1 className="text-7xl text-white  tracking-widest">Sign In</h1>
-            <div className="flex flex-col gap-1 w-[75%] justify-center items-start">
-              <label htmlFor="email" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Email:</label>
-               <input className="px-2.5 py-2 w-full placeholder:text-black bg-slate-200 text-white rounded-xl"  placeholder="example@mail.com"  type="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value}) } name="" id="email" />
+            <div className="flex flex-col mt-3 gap-1 w-[75%] justify-center items-start">
+              <label htmlFor="email" className="block mb-2 text-3xl font-medium text-gray-900 dark:text-white">Email:</label>
+               <input className="px-2.5 py-2 w-full placeholder:text-black bg-slate-300 text-2xl rounded-xl"  placeholder="example@mail.com"  type="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value}) } name="" id="email" />
             </div>
-            <div className="flex flex-col gap-1 w-[75%] justify-center items-start">
-              <label htmlFor="password" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Password:</label>
-              <input className="px-2.5 py-2 w-full placeholder:text-black bg-slate-200 text-white rounded-xl"  placeholder="Enter password" type="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value}) } name="" id="password" />
+            <div className="flex flex-col gap-1 mt-4 w-[75%] justify-center items-start">
+              <label htmlFor="password" className="block mb-2 text-3xl font-medium text-gray-900 dark:text-white">Password:</label>
+              <input className="px-2.5 py-2  w-full placeholder:text-black bg-slate-200 text-2xl rounded-xl"  placeholder="Enter password" type="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value}) } name="" id="password" />
             </div>
             
-            <button type="submit" className="text-3xl px-2.5 py-2 rounded-2xl w-[9rem] bg-slate-700  text-white">Sign In</button>
+            <button type="submit" className="text-3xl px-2.5 py-2 rounded-2xl w-[9rem] bg-gradient-to-br from-cyan-200 via-cyan-800 to-cyan-300 hover:bg-gradient-to-tr hover:from-cyan-300 hover:via-cyan-900 hover:to-cyan-400 mt-3  text-white">Sign In</button>
             <div>
             <p className="text-xl text-white tracking-wide">Don&#39;t have an account? <Link className="underline underline-offset-4 text-2xl text-sky-200" href={'/reg'}>Sign up now!</Link></p>
             </div>
