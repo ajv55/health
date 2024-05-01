789680047753e08087dd65a30dda0b6b86a5bfe7
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-05-01 14:28:29 -0400

    updated the potential weight loss component

---
+++ app/api/getFood/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY!
+})
+
+export async function GET(){
+
+    const res = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{role: 'system', content: `You're a nutritionist expert here to give JSON formatted responses. This response will be for the most common foods and snacks eaten, along with nutritional facts` + 'the final response will always be in a JSON format string ready to be parse following this schema: [foods: {name: , protein: , carbs: , fat: , caloriesPerServingSize: , servingSize: , } ]'}, {role: 'user', content: 'Give me a list of 50 of the most common health fruits different serving size'}],
+        response_format: {type: 'json_object'},
+    })
+
+    const data = res?.choices[0].message.content
+
+    return NextResponse.json({data})
+}
