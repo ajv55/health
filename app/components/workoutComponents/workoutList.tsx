d64530113b61e272bbc324a9f7b714b43bda40c5
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-05-07 17:39:26 -0400

    created the draggable functionality

--- app/components/workoutComponents/workoutList.tsx
+++ app/components/workoutComponents/workoutList.tsx
@@ -1,12 +1,12 @@
 'use client';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction" // needed for dayClick
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/app/store';
-import {setList, setModalOpen, setWorkoutData, setSelectWorkout, setDeleteModal} from '@/app/slices/workoutSlice';
+import {setList, setModalOpen, setWorkoutData, setSelectWorkout, setDeleteModal, setWorkoutList} from '@/app/slices/workoutSlice';
 import { useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -22,19 +22,41 @@ export default function WorkoutList() {
     const deleteModal = useSelector((state: RootState) => state.workout.deleteModal);
     const eventList = useSelector((state: RootState) => state.workout.list);
     const workoutData = useSelector((state: RootState) => state.workout.workoutData);
-    const selectWorkout = useSelector((state:RootState) => state.workout.selectWorkout)
+    const selectWorkout = useSelector((state:RootState) => state.workout.selectWorkout);
+    const workoutListObj = useSelector((state: RootState) => state.workout.workoutList);
+    const workoutList = Object.values(workoutListObj!); // Convert object to array
 
     const getEvents = async () => {
         return axios.get('/api/getEvents').then((res: any) => dispatch(setList(res?.data?.res)));
     }
 
     const getWorkoutList = async () => {
-      return await axios.get('/api/getWorkoutList').then((res) => console.log(res?.data?.workout?.medium_intensity))
+      return await axios.get('/api/getWorkoutList').then((res) => dispatch(setWorkoutList(res?.data?.workout?.medium_intensity)))
     }
 
     useEffect(() => {
         getEvents();
         getWorkoutList();
+
+        
+        let draggableEl = document.getElementById('drag');
+        if(draggableEl) {
+          new Draggable(draggableEl, {
+            itemSelector: '.fc-workout',
+            eventData: function (eventEl){
+              const exercises = eventEl.getAttribute('data-exercise')
+              console.log(exercises)
+              return {
+                extendedProps: {
+                  exercise: exercises
+                }
+              };
+            }
+          })
+        }
+
+
+
     }, [])
 
     const handleDateClick = (arg: any) => {
@@ -48,13 +70,13 @@ export default function WorkoutList() {
         console.log('event: ', event?.event?._def?.publicId)
       }
 
-      console.log(selectWorkout)
+      
 
       const eventRender = (info: any) => {
         return (
           <div className=' bg-teal-500 p-2 rounded-md overflow-scroll'>
             <p className=''>{info?.event?.extendedProps?.muscle}</p>
-            <p>Exercise: {info?.event?.extendedProps?.exercise}</p>
+            <p className=''>Exercise: {info?.event?.extendedProps?.exercise}</p>
           </div>
         );
       };
@@ -83,8 +105,17 @@ export default function WorkoutList() {
           />
       </div>
 
-      <div className='w-[25%]  h-[32rem] bg-slate-400 rounded-lg' >
-
+      <div id='drag'  className='w-[25%]  h-[32rem] bg-slate-400 rounded-lg' >
+        {workoutList.map((wl, i: number) => {
+          const exerciseList = wl?.exercise
+          const names = exerciseList.map((el: any) =>  el.name);
+        return (
+          <div data-exercise={names}  className='fc-workout w-full h-22 flex flex-wrap justify-start items-center overflow-scroll rounded-2xl bg-violet-500' key={i}>
+            <h1 id={names} className='text-3xl  font-bold'>{names}</h1>
+          </div>
+        )
+      })
+}
       </div>
     </div>
   )
