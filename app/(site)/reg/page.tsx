3b6a25215114e76d81ecd61c7e2687554cefa012
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-05-24 02:33:13 -0400

    update the register page

--- app/(site)/reg/page.tsx
+++ app/(site)/reg/page.tsx
@@ -8,6 +8,7 @@ import WeightInput from "@/app/components/input";
 import HeightInput from "@/app/components/heightInput";
 import {useSession} from 'next-auth/react'
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 
 export default function Page() {
@@ -40,37 +41,37 @@ export default function Page() {
     console.log(data)
 
   return (
-    <div  className='w-full  h-screen bg-cover bg-center flex  relative justify-start items-center '>
+    <div  className='w-full  lg:h-screen h-content bg-cover bg-center flex lg:flex-row flex-col relative justify-start items-center '>
 
-        <div style={{backgroundImage: 'linear-gradient(to left, rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url("/registerpage.jpg")'}} className="w-[87%] absolute h-screen right-0 bg-cover bg-center flex  justify-end items-center">
-            <div className="w-[54%]  flex flex-col justify-between items-start p-1 h-screen">
-               <h2 className="text-6xl text-white text-center  font-bold tracking-wide">Unlock Your <span className="font-extrabold text-7xl bg-gradient-to-tl from-stone-200 via-orange-600 to-red-900 bg-clip-text text-transparent">Fitness Potential</span> and Join the FitGenius Community Today!</h2>
+        <div style={{backgroundImage: 'linear-gradient(to left, rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url("/registerpage.jpg")'}} className="lg:w-[87%] w-full absolute lg:h-screen h-[64rem] lg:right-0 bg-cover bg-center flex  justify-end items-center">
+            <div className="w-[54%] hidden  lg:flex flex-col justify-between items-start p-1 h-screen">
+               <h2 className="text-6xl  text-white text-center  font-bold tracking-wide">Unlock Your <span className="font-extrabold text-7xl bg-gradient-to-tl from-stone-200 via-orange-600 to-red-900 bg-clip-text text-transparent">Fitness Potential</span> and Join the FitGenius Community Today!</h2>
                <p className="text-2xl text-white text-center tracking-wide">Ready to embark on an <span className="font-bold text-3xl bg-gradient-to-tr from-red-300 via-amber-200 to-red-600 bg-clip-text text-transparent">exhilarating journey</span> to a fitter, healthier you? Join thousands of like-minded individuals who are <span className="text-4xl font-extrabold bg-gradient-to-tl from-orange-700 via-amber-300 to-stone-400 bg-clip-text text-transparent">transforming their lives</span> with FitGenius. With <span className="font-extrabold text-3xl bg-gradient-to-bl from-teal-700 via-teal-200 to-teal-400 bg-clip-text text-transparent">personalized</span> workout plans, expert <span className="font-extrabold text-3xl bg-gradient-to-bl from-emerald-800 via-emerald-200 to-teal-700 bg-clip-text text-transparent">nutrition </span> guidance, and a supportive community by your side, reaching your fitness goals has never <span className="font-extrabold text-3xl bg-gradient-to-br from-amber-700 via-amber-200 to-purple-400 bg-clip-text text-transparent">been more exciting</span>. Don&#39;t miss out &#45; <span className="underline underline-offset-4 text-3xl text-white" >Sign Up Now!</span> and let&#39;s make fitness fun <span className="font-bold text-3xl">!</span></p>
                <div className="w-full p-2  h-[3rem] flex justify-end items-end">
                   <Link className="hover:underline hover:underline-offset-8 text-2xl text-white tracking-widest" href='/'>Home</Link>
                </div>
             </div>
-
+            <Link className="text-white font-light self-start  text-lg p-2 " href='/'>Home</Link>
         </div>
 
-        <form className=" rounded-r-2xl bg-white z-10 w-[53%] drop-shadow-2xl h-screen p-4 flex  flex-wrap justify-center items-center gap-6" ref={ref} onSubmit={handleSubmit}>
+        <motion.form variants={{hidden: {y: '-100vh', opacity: 0}, visible: {y: 0, opacity: 1}}} initial='hidden' animate='visible' transition={{duration: 1, type: 'spring', stiffness: 80 }} className=" lg:rounded-r-2xl rounded-2xl mt-10 lg:mt-0 bg-white z-10 lg:w-[53%] w-[97%] drop-shadow-2xl lg:h-screen h-content p-4 flex  flex-wrap justify-center items-center gap-6" ref={ref} onSubmit={handleSubmit}>
         <h1 className=" bg-gradient-to-tr from-emerald-800 p-2 via-orange-500 to-emerald-50 bg-clip-text text-transparent text-7xl w-full text-center font-bold tracking-wide">Sign Up</h1>
-            <div className="flex  w-[48%] flex-col justify-start items-start gap-2">
-                <label className="text-2xl font-bold tracking-wider text-zinc-800" htmlFor="name">Username</label>
-               <input className="px-2.5 py-2 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full text-2xl  rounded-md placeholder:text-zinc-400 placeholder:text-2xl focus:outline-none outline-none" placeholder="Enter name" type="text" value={data.name} onChange={(e) => setData({...data, name: e.target.value}) } name="" id="name" />
+            <div className="flex  lg:w-[48%] w-full flex-col justify-start items-start gap-2">
+                <label className="lg:text-2xl text-lg font-bold tracking-wider text-zinc-800" htmlFor="name">Username</label>
+               <input className="px-2.5 py-2 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full text-2xl  rounded-md placeholder:text-zinc-400 lg:placeholder:text-2xl placeholder:text-lg focus:outline-none outline-none" placeholder="Enter name" type="text" value={data.name} onChange={(e) => setData({...data, name: e.target.value}) } name="" id="name" />
             </div>
-            <div className="flex  w-[48%] flex-col justify-start items-start gap-2">
-                <label className="text-2xl font-bold tracking-wider text-zinc-800" htmlFor="passowrd">Password</label>
-                <input className="px-2.5 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 text-2xl py-2 w-full rounded-md placeholder:text-zinc-400 focus:outline-none placeholder:text-2xl  outline-none" placeholder="Enter password" type="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value}) } name="" id="password" />
+            <div className="flex  lg:w-[48%] w-full flex-col justify-start items-start gap-2">
+                <label className="lg:text-2xl text-lg font-bold tracking-wider text-zinc-800" htmlFor="passowrd">Password</label>
+                <input className="px-2.5 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 text-2xl py-2 w-full rounded-md placeholder:text-zinc-400 focus:outline-none lg:placeholder:text-2xl placeholder:text-lg  outline-none" placeholder="Enter password" type="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value}) } name="" id="password" />
             </div>
-            <div className="flex w-[57%] flex-col justify-start items-start gap-2">
-                <label className="text-2xl font-bold tracking-wider text-zinc-800" htmlFor="email">Email</label>
-                <input className="px-2.5 py-2 text-2xl border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full rounded-xl placeholder:text-zinc-400 placeholder:text-2xl   focus:outline-none outline-none" placeholder="example@mail.com"  type="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value}) } name="" id="email" />
+            <div className="flex lg:w-[85%] w-full flex-col justify-start items-start gap-2">
+                <label className="lg:text-2xl text-lg font-bold tracking-wider text-zinc-800" htmlFor="email">Email</label>
+                <input className="px-2.5 py-2 text-2xl border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full rounded-md placeholder:text-zinc-400 lg:placeholder:text-2xl placeholder:text-lg   focus:outline-none outline-none" placeholder="example@mail.com"  type="email" value={data.email} onChange={(e) => setData({...data, email: e.target.value}) } name="" id="email" />
             </div>
 
-           <div className=" w-[18%]  flex flex-col justify-center gap-2 items-start">
-               <label className="text-zinc-800  text-2xl font-bold tracking-wider" htmlFor="gender">Gender</label>
-               <select  value={data.gender} onChange={(e) => setData({...data, gender: e.target.value})} className="w-full px-2.5 py-2 text-zinc-800 placeholder:text-zinc-400 border-b-2 border-r-2 drop-shadow-2xl border-zinc-800  rounded-2xl outline-none  text-2xl" name="" id="gender">
+           <div className=" lg:w-[38%] w-[43%]  flex flex-col justify-center gap-2 items-start">
+               <label className="text-zinc-800  lg:text-2xl text-lg font-bold tracking-wider" htmlFor="gender">Gender</label>
+               <select  value={data.gender} onChange={(e) => setData({...data, gender: e.target.value})} className="w-full px-2.5 py-2 text-zinc-800 placeholder:text-zinc-400 border-b-2 border-r-2 drop-shadow-2xl border-zinc-800  rounded-md outline-none  lg:text-2xl text-lg" name="" id="gender">
                    <option disabled value="">Choose an gender</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
@@ -78,9 +79,9 @@ export default function Page() {
            </div>
 
             
-            <div className="flex  flex-col justify-start items-start gap-2">
-                <label className="text-2xl font-bold tracking-wider text-zinc-800" htmlFor="age">Age</label>
-                <input className="px-2.5 py-2 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full rounded-md placeholder:text-zinc-400 placeholder:text-2xl  text-xl outline-none" placeholder="Enter age" type="text" value={data.age} onChange={(e) => setData({...data, age: e.target.value}) } name="" id="age" />
+            <div className="flex lg:w-[45%] w-[43%] flex-col justify-start items-start gap-2">
+                <label className="lg:text-2xl text-lg font-bold tracking-wider text-zinc-800" htmlFor="age">Age</label>
+                <input className="px-2.5 py-2 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full rounded-md placeholder:text-zinc-400 lg:placeholder:text-2xl placeholder:text-lg   text-xl outline-none" placeholder="Enter age" type="text" value={data.age} onChange={(e) => setData({...data, age: e.target.value}) } name="" id="age" />
             </div>
             
             <WeightInput value={data.weightInLbs} onChange={(e: any) => setData({...data, weightInLbs: e.target.value})}/>
@@ -88,8 +89,8 @@ export default function Page() {
 
             <div className=" flex justify-evenly items-center">
                 <div className=" w-[45%] h-[3rem] flex flex-col justify-start items-start gap-2">
-                    <label className="text-zinc-800  text-xl font-bold tracking-wider" htmlFor="activity">Current Physical Activity Level</label>
-                    <select value={data.TDEE} className="w-full px-2.5 py-2 text-zinc-800 placeholder:text-zinc-400 border-b-2 border-r-2 drop-shadow-md border-zinc-800  rounded-2xl outline-none  text-2xl" onChange={(e) => setData({...data, TDEE: e.target.value})} name="activity" id="activity">
+                    <label className="text-zinc-800  lg:text-xl text-lg font-bold tracking-wider" htmlFor="activity">Physical Activity</label>
+                    <select value={data.TDEE} className="w-full px-2.5 py-2 text-zinc-800  border-b-2 border-r-2 drop-shadow-md border-zinc-800  rounded-md outline-none  lg:text-2xl text-lg" onChange={(e) => setData({...data, TDEE: e.target.value})} name="activity" id="activity">
                         <option disabled value="">Activity</option>
                         <option value="No-Exercise">Little to no exercise and work a desk job</option>
                         <option value="Light-Exercise">Light exercise 1-3 days per week</option>
@@ -100,8 +101,8 @@ export default function Page() {
                 </div>
 
                 <div className=" w-[45%] h-[3rem] flex flex-col justify-start items-start gap-1">
-                    <label className="text-xl font-bold tracking-wider text-zinc-800" htmlFor="goal">Goal Weight</label>
-                    <input className="px-2.5 py-2 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full text-2xl  rounded-xl placeholder:text-zinc-400 placeholder:text-2xl focus:outline-none outline-none" placeholder="Your goal weight..." type="text" value={data.goal} onChange={(e) => setData({...data, goal: e.target.value}) } name="" id="goal" />  
+                    <label className="lg:text-xl text-lg font-bold tracking-wider text-zinc-800" htmlFor="goal">Goal Weight</label>
+                    <input className="px-2.5 py-2 border-b-2 border-r-2 drop-shadow-lg border-zinc-800 w-full text-2xl  rounded-md placeholder:text-zinc-400 lg:placeholder:text-2xl placeholder:text-lg focus:outline-none outline-none" placeholder="Your goal weight..." type="text" value={data.goal} onChange={(e) => setData({...data, goal: e.target.value}) } name="" id="goal" />  
                 </div>
             </div>
 
@@ -109,16 +110,16 @@ export default function Page() {
             <div className="flex flex-col justify-center items-start mt-7 gap-2 ">
               <div className="flex justify-center items-center gap-2">
                 <input className="border w-4 h-4 cursor-pointer " value={data.agree} checked={data.agree === 'agreed'} onChange={(e) => setData({...data, agree: 'agreed'})} name="terms" id="terms" type="radio"  />
-                <label className="text-xl" htmlFor="terms">I agree to the <span className="font-bold underline underline-offset-4 text-blue-600 cursor-pointer"> Terms of Service</span>  and  <span className="font-bold underline underline-offset-4 text-blue-600 cursor-pointer"> Privacy Policy</span>.</label>
+                <label className="lg:text-xl text-sm" htmlFor="terms">I agree to the <span className="font-bold underline underline-offset-4 text-blue-600 cursor-pointer"> Terms of Service</span>  and  <span className="font-bold underline underline-offset-4 text-blue-600 cursor-pointer"> Privacy Policy</span>.</label>
                
               </div>
-              <p className="text-center text-lg tracking-wide">Already have an account? <Link href='/login' className="font-bold text-lg underline underline-offset-4 cursor-pointer text-blue-600">Sign In</Link></p>
+              <p className="text-center lg:text-lg text-md tracking-wide">Already have an account? <Link href='/login' className="font-bold text-lg underline underline-offset-4 cursor-pointer text-blue-600">Sign In</Link></p>
             </div>
             
             
             
-            <button type="submit" className="text-3xl bg-gradient-to-br from-orange-900 via-orange-500 to-orange-500 hover:bg-gradient-to-tl hover:from-emerald-800 hover:via-teal-500 hover:to-teal-200   w-[75%] rounded-2xl px-2 py-3 bg-slate-700 text-white tracking-widest drop-shadow-xl">Create Account</button>
-        </form>
+            <button type="submit" className="lg:text-3xl text-2xl bg-gradient-to-br from-orange-900 via-orange-500 to-orange-500 hover:bg-gradient-to-tl hover:from-emerald-800 hover:via-teal-500 hover:to-teal-200   w-[75%] rounded-2xl px-2 py-3 bg-slate-700 text-white tracking-widest drop-shadow-xl">Create Account</button>
+        </motion.form>
 
     </div>
   )
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,13 +1,65 @@
+'use client';
 import Link from 'next/link'
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '@/public/FitGenius_transparent.png'
 import { FiActivity } from "react-icons/fi";
 import Signing from './mainPage/signing';
 import { RiMenu3Line } from "react-icons/ri";
+import { motion } from 'framer-motion';
+import { useSession } from 'next-auth/react';
 
 export default function Nav() {
+
+  const {data: session} = useSession();
+
+  const userName = session?.user?.name
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  console.log(isOpen)
+
   return (
-    <div className='lg:w-full bg-transparent w-full   z-10 absolute top-0 lg:h-content py-3 flex justify-between items-center p-3'>
+    <div className='lg:w-full bg-transparent w-full absolute  top-0 left-0 lg:h-content py-3 flex justify-between items-center p-3'>
+      {isOpen && (
+                <motion.div
+                    initial={{ x: '-100vw' }}
+                    animate={{ x: 0 }}
+                    transition={{ type: 'spring', stiffness: 50 }}
+                    className="fixed top-0 left-0 w-64 h-screen bg-white flex flex-col justify-between items-start shadow-lg z-50"
+                >
+                    <nav className="flex flex-col p-4 space-y-8">
+                        <Link onClick={() => setIsOpen(!isOpen)} className="text-2xl font-semibold text-gray-800 hover:text-teal-500" href="/">
+                         Home
+                        </Link>
+                        <Link onClick={() => setIsOpen(false)} className="text-2xl font-semibold text-gray-800 hover:text-teal-500" href="/About">
+                        About
+                        </Link>
+                        <Link onClick={() => setIsOpen(false)} className="text-2xl font-semibold text-gray-800 hover:text-teal-500" href="/pricing">
+                        Pricing
+                        </Link>
+                        <Link onClick={() => setIsOpen(false)} className="text-2xl font-semibold text-gray-800 hover:text-teal-500" href="/Contact">
+                        Contact
+                        </Link>
+                    </nav>
+                    {isLoggedIn ? (
+                      <div className="flex flex-col items-start space-y-4">
+                        <span className="text-lg text-gray-800">Welcome, {userName}</span>
+                        <Link href='/signOut' className="text-lg text-gray-800 hover:text-teal-500">Logout</Link>
+                      </div>
+                    ) : (
+                      <div className="flex p-4 w-full flex-col items-start space-y-4">
+                        <Link href='/login' className="text-2xl font-semibold text-blue-600 hover:text-teal-500">Login</Link>
+                        <Link href='/reg' className="text-2xl font-semibold text-blue-600 hover:text-teal-500">Sign Up</Link>
+                      </div>
+                    )}
+
+                    <div className=' lg:w-full lg:mt-0 mt-8 w-[97%] h-[4rem] flex justify-center items-center'>
+                        <p className='text-lg self-end text-center lg:text-right'>Copyright © 2024 FitGenius. All rights reserved.</p>
+                    </div>
+                </motion.div>
+            )}
+
         <div className='flex justify-center items-center gap-2'>
            <h1 className='text-4xl text-white'>FitGenius</h1>
            <FiActivity size={30} color='gold'/>
@@ -18,7 +70,7 @@ export default function Nav() {
             <Link className='  hover:text-2xl hover:before:scale-x-100 hover:before:origin-right relative before:w-full before:h-0.5 before:origin-right before:transition-transform before:duration-300 before:scale-x-0 before:bg-white before:absolute before:left-0 before:bottom-0' href='/Contact'>Contact</Link>
             <Link className='  hover:text-2xl hover:before:scale-x-100 hover:before:origin-right relative before:w-full before:h-0.5 before:origin-right before:transition-transform before:duration-300 before:scale-x-0 before:bg-white before:absolute before:left-0 before:bottom-0' href='/About'>About</Link>
         </nav>
-        <RiMenu3Line className='lg:hidden' size={30} color='white' />
+        <RiMenu3Line onClick={() => setIsOpen(!isOpen)} className='lg:hidden' size={30} color='white' />
         <Signing />
     </div>
   )
