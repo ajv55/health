08ddbbd10d35e424af6f02733b7f6723962f5a05
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-07-23 02:18:44 -0400

    updates to pricing page

--- app/components/pricingComponent.tsx/pricingCards.tsx
+++ app/components/pricingComponent.tsx/pricingCards.tsx
@@ -46,16 +46,16 @@ export default function PricingCards() {
         <div className="flex flex-wrap justify-center">
 
             {/* <!-- Free Plan --> */}
-            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg shadow-zinc-900 bg-white m-4">
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg shadow-indigo-900 ring-2 ring-indigo-400 bg-white m-4">
                 <div className="px-6 flex flex-col justify-center items-start gap-3 py-4">
-                    <div className="font-bold text-3xl mb-2">Free Plan</div>
+                    <div className="font-bold text-indigo-600 text-3xl mb-2">Free Plan</div>
                     <p className="text-gray-700 text-4xl mb-4">$0 / month</p>
                     <p className="text-gray-700 text-lg mb-4">Perfect for those who are just getting started on their fitness journey.</p>
                     <ul className=" list-disc list-inside text-gray-700 text-base mb-4">
-                        <li><strong>Calorie Tracking:</strong> Track your daily calorie intake with ease.</li>
-                        <li><strong>Basic Workout Tracking:</strong> Log your workouts and monitor your progress.</li>
-                        <li><strong>Nutritional Guide:</strong> Access to a common nutritional guide for healthy eating.</li>
-                        <li><strong>Maintenance Calories Calculation:</strong> Get your daily maintenance calories by providing age, weight, height, and activity level.</li>
+                        <li><strong className='text-indigo-600'>Calorie Tracking:</strong> Track your daily calorie intake with ease.</li>
+                        <li><strong className='text-indigo-600'>Basic Workout Tracking:</strong> Log your workouts and monitor your progress.</li>
+                        <li><strong className='text-indigo-600'>Nutritional Guide:</strong> Access to a common nutritional guide for healthy eating.</li>
+                        <li><strong className='text-indigo-600'>Maintenance Calories Calculation:</strong> Get your daily maintenance calories by providing age, weight, height, and activity level.</li>
                     </ul>
                     <div className="flex justify-center">
                         <Link href='/reg' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up for Free</Link>
@@ -64,24 +64,26 @@ export default function PricingCards() {
             </div>
 
             {/* <!-- Pro Plan --> */}
-            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg shadow-zinc-900 bg-white m-4">
-                <div className="px-6 flex flex-col justify-center items-start gap-3 py-4">
-                    <div className="font-bold text-3xl mb-2">Pro Plan</div>
-                    <p className="text-gray-700 text-4xl mb-4">$9.99 / month</p>
-                    <p className="text-gray-700 text-lg mb-4">Ideal for individuals serious about their fitness goals and looking for advanced features.</p>
-                    <ul className="list-disc list-inside text-gray-700 text-base mb-4">
-                        <li>All Free Plan Features</li>
-                        <li><strong>Advanced Calorie Tracking:</strong> Detailed breakdown of macronutrients (proteins, fats, carbs).</li>
-                        <li><strong>Custom Workout Plans:</strong> Personalized workout routines based on your goals and fitness level.</li>
-                        <li><strong>Personalized Nutritional Guide:</strong> Tailored dietary advice and meal plans.</li>
-                        <li><strong>Progress Tracking:</strong> Monitor your weight loss and fitness improvements over time.</li>
-                        <li><strong>Priority Support:</strong> Get quick responses to your queries and issues.</li>
-                    </ul>
-                    <div className="flex justify-center">
-                        <button onClick={handleCreateCheckoutSession} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Start Your 7-Day Free Trial</button>
-                    </div>
-                </div>
-            </div>
+            <div className="max-w-sm rounded-xl overflow-hidden shadow-lg shadow-indigo-900 ring-2 ring-indigo-400 bg-white m-4">
+  <div className="px-6 flex flex-col justify-center items-start gap-3 py-4">
+    <div className="font-bold text-indigo-600 text-3xl mb-2">Pro Plan</div>
+    <p className="text-gray-700 text-4xl mb-4">$9.99 / month</p>
+    <p className="text-gray-700 text-lg mb-4">Ideal for individuals serious about their fitness goals and looking for advanced features.</p>
+    <ul className="list-disc list-inside text-gray-700 text-base mb-4">
+      <li>All Free Plan Features</li>
+      <li><strong className='text-indigo-600'>Advanced Calorie Tracking:</strong> Detailed breakdown of macronutrients (proteins, fats, carbs).</li>
+      <li><strong className='text-indigo-600'>Custom Workout Plans:</strong> Personalized workout routines based on your goals and fitness level.</li>
+      <li><strong className='text-indigo-600'>Progress Tracking:</strong> Monitor your weight loss and fitness improvements over time.</li>
+      <li><strong className='text-indigo-600'>30-Day Analytics:</strong> Comprehensive analytics for a 30-day period to track your progress.</li>
+      <li><strong className='text-indigo-600'>Daily Reports:</strong> Receive daily reports on your nutrition facts and macronutrient intake (via pdf file).</li>
+      <li><strong className='text-indigo-600'>Priority Support:</strong> Get quick responses to your queries and issues.</li>
+    </ul>
+    <div className="flex justify-center">
+      <button onClick={handleCreateCheckoutSession} className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">Start Your 7-Day Free Trial</button>
+    </div>
+  </div>
+</div>
+
 
         </div>
     </div>
